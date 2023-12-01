4deb712a3cbb63bb7ba274e83050fd809f0f90cf
ArthurPanzera13 <arthurpanzera@outlook.com>
2023-12-01 18:18:01 -0300

    versionamento

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home/>}/>
+          <Route path='/CatalogoDeFilmes' element={<Home/>}/>
           <Route path='details/:id' element={<Details />}/>
         </Routes>
       </BrowserRouter>
